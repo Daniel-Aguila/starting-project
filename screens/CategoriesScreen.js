012c2aa0f31bb8262bcb530ba60e1e53796fbf15
Daniel Aguila <danielbaguila@gmail.com>
2024-11-07 21:42:58 -0600

    skeletal

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,19 @@
+import {CATEGORIES} from '../data/dummy-data';
+import { FlatList } from 'react-native-web';
+import CategoryGridTitle from '../components/CategoryGridTitle';
+
+function renderCategoryItem(itemData){
+    return <CategoryGridTitle title={itemData.item.title} color={itemData.item.color}/>
+}
+
+function CategoriesScreen(){
+    return 
+    (
+    <FlatList data={CATEGORIES} 
+    keyExtractor={(item) => item.id} 
+    renderItem={renderCategoryItem}
+    />
+    );
+}
+
+export default CategoriesScreen;
